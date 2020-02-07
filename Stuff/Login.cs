7f63e23347cd6ea934fa2dbf599602f34b7e9a43
apexMadeIt <60795070+apexMadeIt@users.noreply.github.com>
2020-02-07 18:26:40 -0500

    Add files via upload

---
+++ Stuff/Login.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Credintials
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private int attempts;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = "admin";
+            string pass = "admin";
+            if (textBox1.Text == user && textBox2.Text == pass)
+            {
+                attempts = 0;
+                MessageBox.Show("Login Success");
+                Main f2 = new Main();
+                f2.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect Credintials");
+                attempts++;
+                return;
+            }
+            if (attempts == 3)
+            {
+                MessageBox.Show("fuck");
+                Close();
+            }
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Stuff/Main.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace Credintials
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Random String
+        {
+            char[] letters = "qwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*()".ToCharArray();
+            Random rand = new Random();
+            string randomstring = "";
+
+            for (int i = 0; i < 20; i++)
+            {
+                randomstring += letters[rand.Next(20, 30)].ToString();
+            }
+            textBox4.Text = (randomstring.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
+            string query = "INSERT into creds.cool(website, username, password) VALUES('" + textBox1.Text + "', '" + textBox2.Text + "', '" + textBox4.Text + "')"; //Make sure you change the db and table name to your own
+
+            connection.Open();
+            MySqlCommand command = new MySqlCommand(query, connection);
+
+            try
+            {
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Success Submitted");
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            connection.Close();
+        }
+    }
+}
